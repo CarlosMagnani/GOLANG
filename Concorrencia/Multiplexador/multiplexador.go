330a7f8e2d4e038d370cdb9dec74d3f39b39a65b
= <=>
2023-11-09 20:11:38 -0300

    feat: multiplex pattern

---
+++ Concorrencia/Multiplexador/multiplexador.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+func main() {
+	canal :=  multiplex(escrever("Olá Mundo"), escrever("Programando em go"))
+	for i := 0; i < 10; i++ {
+		fmt.Printf(<-canal)
+	}
+}
+// EU UTILIZO TAMBÉM O CONCEITO DE GENERATOR DENTOR DO MULTIPEXADOR PARA FAZER A CRIAÇÃO DE CANAIS E
+// GOROUTINES
+func multiplex(canalDeEntrada, canalDeEntrada2 <-chan string) <-chan string {
+	canalDeSaida := make(chan string)
+	go func() {
+		for {
+			select {
+			case mensagem := <-canalDeEntrada:
+				canalDeSaida <- mensagem
+
+			case mensagem2 := <-canalDeEntrada2:
+				canalDeSaida <- mensagem2
+			}
+		}
+	}()
+
+	return canalDeSaida
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func(){
+		//AQUI FAZEMOS UM LOOP PARA MANDAR AS MENSAGENS PARA O CANAL
+		for {
+			canal <- fmt.Sprintf("Valor recebido: %s", texto)
+			time.Sleep(time.Millisecond * 500)
+		}
+	}();
+
+	
+	return canal
+}
